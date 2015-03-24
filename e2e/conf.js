9ae4377bc239c15ac91595303c2a59c05d251e40
DanLDB <dpbergquist>
2015-03-24 17:02:03 +0000

    remove seleniumAddress

--- e2e/conf.js
+++ e2e/conf.js
@@ -1,12 +1,13 @@
 exports.config = {
-  seleniumAddress: connection_url(),
-  specs: ['search-spec.js']
+  specs: ['search-spec.js'],
+  sauceUser: process.env.SAUCE_USERNAME,
+  sauceKey: process.env.SAUCE_ACCESS_KEY
 };
 
-function connection_url() {
-  if(process.env.SAUCE_USERNAME) {
-    return "http://" + process.env.SAUCE_USERNAME + ":" + process.env.SAUCE_ACCESS_KEY + "@" + process.env.WDADDRESS
-  } else {
-    return "http://localhost:4444/wd/hub"
-  }
-}
+// function connection_url() {
+//   if(process.env.SAUCE_USERNAME) {
+//     return "http://" + process.env.SAUCE_USERNAME + ":" + process.env.SAUCE_ACCESS_KEY + "@" + process.env.WDADDRESS
+//   } else {
+//     return "http://localhost:4444/wd/hub"
+//   }
+// }
