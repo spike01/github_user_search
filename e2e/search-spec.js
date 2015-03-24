bc69aca5380bbb160ef8fa74939ddc760998e2d6
DanLDB <dpbergquist>
2015-03-24 17:59:01 +0000

    add waitForAngular

--- e2e/search-spec.js
+++ e2e/search-spec.js
@@ -1,52 +1,12 @@
 describe('Github user search', function() {
 
-  describe('angularjs homepage', function() {
-
-   it('should greet the named user', function() {
-     browser.get('http://www.angularjs.org');
-
-     element(by.model('yourName')).sendKeys('Julie');
-
-     var greeting = element(by.binding('yourName'));
-
-     expect(greeting.getText()).toEqual('Hello Julie!');
-   });
-
-   describe('todo list', function() {
-     var todoList;
-
-     beforeEach(function() {
-       browser.get('http://www.angularjs.org');
-
-       todoList = element.all(by.repeater('todo in todos'));
-     });
-
-     it('should list todos', function() {
-       expect(todoList.count()).toEqual(2);
-       expect(todoList.get(1).getText()).toEqual('build an angular app');
-     });
-
-     it('should add a todo', function() {
-       var addTodo = element(by.model('todoText'));
-       var addButton = element(by.css('[value="add"]'));
-
-       addTodo.sendKeys('write a protractor test');
-       addButton.click();
-
-       expect(todoList.count()).toEqual(3);
-       expect(todoList.get(2).getText()).toEqual('write a protractor test');
-     });
-   });
-  });
-
-  describe('ourtests', function(){
-
     beforeEach(function() {
-      browser.get('http://localhost:3000')
+      browser.get('http://localhost:3000');
+      browser.waitForAngular();
     });
 
     it('has a title', function() {
       expect(browser.getTitle()).toEqual('Github user search')
     });
-  });
+    
 });
