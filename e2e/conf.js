bfa943eda7ce485e63d74dca02e1bc8e4aa77365
Daniel Le Dosquet-Bergquist <dpbergquist@gmail.com>
2015-04-26 10:39:03 +0100

    remove sauce from protractor config

--- e2e/conf.js
+++ e2e/conf.js
@@ -1,8 +1,5 @@
 exports.config = {
   specs: ['search-spec.js'],
-  sauceUser: process.env.SAUCE_USERNAME,
-  sauceKey: process.env.SAUCE_ACCESS_KEY,
-  sauceSeleniumAddress: 'localhost:4445/wd/hub',
   capabilities: {
      'browserName': 'chrome'
   },
